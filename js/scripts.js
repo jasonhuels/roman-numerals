da1ae7cf7f1f1f684fcad005540695d4657f2204
Carrie Schmid and Jason Huels <jason@>
2019-08-22 11:03:27 -0700

    rewrite the roman numeral converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,26 @@
-function romanNumerals(inputNumber) {
-  const ROMAN = ["I", "V", "X", "L", "C", "D", "M"];
-  const DECIMAL = [1, 5, 10, 50, 100, 500, 1000];
-  var digits = inputNumber.split("");
+
+function romanNumerals (input) {
+  var ones = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  var tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  var cent = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  var kilo = ["M", "MM", "MMM"];
+  var rn = [ones, tens, cent, kilo];
+
+  var digits = input.split("");
   digits.reverse();
   for(var i=0; i<digits.length; i++) {
     digits[i] = parseInt(digits[i]);
   }
   var output = [];
 
-  // for(var i=0; i<=digits[0]; i++) {
-  //   // if(digits[i] + Math.pow(10, i) >= DECIMAL[i]) {
-  //   //
-  //   // }
-  //   if(digits[0] >= DECIMAL[0]) {
-  //     output.push(ROMAN[0]);
-  //     digits[0] -= DECIMAL[0]
-  //   }
-  // }
-
-  if(inputNumber === '0') {
-    output.push(0);
-  } else {
-    while(digits[0] > 0) {
-      output.push(ROMAN[0]);
-      digits[0] -= DECIMAL[0];
+  if(parseInt(input) <= 3999) {
+    for(var i=0; i<digits.length; i++) {
+      output.push(rn[i][digits[i]-1]);
     }
+  } else {
+    return "Roman Numerals only go up to 3,999."
   }
 
   output = output.reverse().join("");
-
   return output;
 }
